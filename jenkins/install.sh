e3b7f18593c493d70160eaa2db99c73864bbe178
ievgen degtiarenko <ievgen.degtiarenko@gmail.com>
2019-03-03 15:28:57 +0100

    add missing roll for jenkins to manipulate k8s resources

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 kubectl apply -f jenkins.yaml
 helm install --name jenkins --namespace jenkins -f demo-values.yaml stable/jenkins
+kubectl create clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:default
